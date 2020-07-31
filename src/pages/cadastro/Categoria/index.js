cf8378c83b90dd20546b510a1b72db7cdbec4f95
perrylopes <perryalencar@gmail.com>
2020-07-30 21:36:46 -0300

    Atualizacao URL__TOP  com localhost ou heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,7 +32,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'https://pyfly.herokuapp.com/categoria';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categoria'
+      : 'https://pyfly.herokuapp.com/categoria';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
